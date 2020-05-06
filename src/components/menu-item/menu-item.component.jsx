33b144fd8703734945de596a7b44160050dec29c
Leonid <golani04@gmail.com>
2020-05-06 11:33:41 +0300

    style menu items, add bg effect using transition

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => (
-	<div className="menu-item">
+const MenuItem = ({ title, imageURL, size }) => (
+	<div className={`menu-item${size ? ` ${size}` : ''}`}>
+		<div
+			style={{
+				backgroundImage: `url(${imageURL})`,
+			}}
+			className="background-image"
+		/>
 		<div className="content">
 			<h1 className="title">{title}</h1>
 			<span className="subtitle">SHOP NOW</span>
