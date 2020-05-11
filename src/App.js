ed0349bee1fbd301bc01a44bb9724dfe56d49788
Leonid <golani04@gmail.com>
2020-05-11 19:48:56 +0300

    route to shop endpoint

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
-
-const GlovesPage = () => (
-	<div>
-		<h1>Gloves page</h1>
-	</div>
-);
 
 function App() {
 	return (
 		<main>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
-				<Route path="/gloves" component={GlovesPage} />
+				<Route path="/shop" component={ShopPage} />
 			</Switch>
 		</main>
 	);
