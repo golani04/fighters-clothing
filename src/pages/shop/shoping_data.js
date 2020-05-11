4bbe4c4e8839991485fc7491e2b2dc990192eeac
Leonid <golani04@gmail.com>
2020-05-11 19:50:28 +0300

    shop page + data that contains products' information

---
+++ src/pages/shop/shoping_data.js
@@ -0,0 +1,153 @@
+/* Used as starting data for ShopPage */
+
+export const SHOP_DATA = [
+	{
+		id: 1,
+		title: 'Gloves',
+		routeName: 'gloves',
+		items: [
+			{
+				id: 1,
+				name: 'AJBXNG',
+				imageUrl: '/images/shop-img/gloves/ajbxng.jpg',
+				price: 25,
+			},
+			{
+				id: 2,
+				name: 'Blue Beanie',
+				imageUrl: '/images/shop-img/gloves/everlast.jpg',
+				price: 18,
+			},
+			{
+				id: 3,
+				name: 'Favitex',
+				imageUrl: '/images/shop-img/gloves/favitex.jpg',
+				price: 35,
+			},
+			{
+				id: 4,
+				name: 'Title',
+				imageUrl: '/images/shop-img/gloves/title.jpg',
+				price: 25,
+			},
+		],
+	},
+	{
+		id: 2,
+		title: 'Shoes',
+		routeName: 'shoes',
+		items: [
+			{
+				id: 5,
+				name: 'Air Jordan',
+				imageUrl: '/images/shop-img/shoes/airjordan.jpg',
+				price: 220,
+			},
+			{
+				id: 6,
+				name: 'Nike Acrnim',
+				imageUrl: '/images/shop-img/shoes/nike-acrnim.jpg',
+				price: 280,
+			},
+			{
+				id: 7,
+				name: 'Nike Red High Tops',
+				imageUrl: '/images/shop-img/shoes/nikes-red.png',
+				price: 160,
+			},
+			{
+				id: 8,
+				name: 'Nike Brown High Tops',
+				imageUrl: '/images/shop-img/shoes/nike-brown.png',
+				price: 160,
+			},
+		],
+	},
+	{
+		id: 3,
+		title: 'Gear',
+		routeName: 'gear',
+		items: [
+			{
+				id: 9,
+				name: 'Red Hand Wraps',
+				imageUrl: '/images/shop-img/gear/hand-wraps.jpg',
+				price: 50,
+			},
+			{
+				id: 10,
+				name: 'Red Headgear',
+				imageUrl: '/images/shop-img/gear/head.jpg',
+				price: 90,
+			},
+			{
+				id: 11,
+				name: 'Heavy bag',
+				imageUrl: '/images/shop-img/gear/heavy-bag.jpg',
+				price: 200,
+			},
+			{
+				id: 12,
+				name: 'White punching mitts',
+				imageUrl: '/images/shop-img/gear/punching-mitts.jpg',
+				price: 125,
+			},
+			{
+				id: 13,
+				name: 'Skipping rope',
+				imageUrl: '/images/shop-img/gear/skipping-rope.jpg',
+				price: 22,
+			},
+			{
+				id: 14,
+				name: 'Weight ball',
+				imageUrl: '/images/shop-img/gear/weight-ball.jpg',
+				price: 30,
+			},
+		],
+	},
+	{
+		id: 4,
+		title: 'Womens',
+		routeName: 'womens',
+		items: [
+			{
+				id: 15,
+				name: 'White Karate Gi',
+				imageUrl: '/images/shop-img/womens/karategi.jpg',
+				price: 100,
+			},
+			{
+				id: 16,
+				name: 'kolby Rae',
+				imageUrl: '/images/shop-img/womens/kolby-rae.jpg',
+				price: 90,
+			},
+			{
+				id: 17,
+				name: 'Addidas',
+				imageUrl: '/images/shop-img/womens/addidas.jpg',
+				price: 110,
+			},
+		],
+	},
+	{
+		id: 5,
+		title: 'Mens',
+		routeName: 'mens',
+		items: [
+			{
+				id: 18,
+				name: 'Jiu Jitsu Gi',
+				imageUrl: '/images/shop-img/mens/jiu-jitsu.jpg',
+				price: 225,
+			},
+			{
+				id: 19,
+				name: 'Kung Fu Uniform',
+				imageUrl: '/images/shop-img/mens/kung-fu.jpg',
+				price: 195,
+			},
+		],
+	},
+];
