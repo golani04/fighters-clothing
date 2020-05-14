137df88c61631dfaf2d3f2865b3cdc28bf387cb8
Leonid <golani04@gmail.com>
2020-05-14 09:40:10 +0300

    change app component to class component in order to store user state

--- src/App.js
+++ src/App.js
@@ -5,20 +5,41 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 
-function App() {
-	return (
-		<main>
-			<Header />
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route path="/shop" component={ShopPage} />
-				<Route path="/signin" component={SignInAndSignUpPage} />
-			</Switch>
-		</main>
-	);
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { currentUser: null };
+	}
+
+	unsubscribeFromAuth = null;
+
+	componentDidMount() {
+		auth.onAuthStateChanged((user) => {
+			this.setState({ currentUser: user });
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+
+	render() {
+		return (
+			<main>
+				<Header currentUser={this.state.currentUser} />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route path="/signin" component={SignInAndSignUpPage} />
+				</Switch>
+			</main>
+		);
+	}
 }
 
 export default App;
