ab90523d9939c2118891d959844a76473c690a54
Leonid <golani04@gmail.com>
2020-05-14 09:41:32 +0300

    show if user is signed in or sign out

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from './../../assets/boxing_gloves.svg';
+import { auth } from '../../firebase/firebase.utils';
+
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
 	<header className="main-header">
 		<Link className="logo-container" to="/">
 			<Logo className="logo" />
@@ -16,6 +18,15 @@ const Header = () => (
 			<Link className="option" to="/contact">
 				contact
 			</Link>
+			{currentUser ? (
+				<div className="option" onClick={() => auth.signOut()}>
+					Sign out
+				</div>
+			) : (
+				<Link className="option" to="signin">
+					Sign in
+				</Link>
+			)}
 		</nav>
 	</header>
 );
